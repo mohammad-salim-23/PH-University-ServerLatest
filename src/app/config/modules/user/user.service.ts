7f9b2cb8249066a1d31be7414348bb9bd8581cb3
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-12-25 13:13:54 +0600

    createAdmIntoDB in user.service.ts file

--- src/app/config/modules/user/user.service.ts
+++ src/app/config/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose from "mongoose";
 import config from "../..";
@@ -7,12 +8,14 @@ import { TStudent } from "../student/student.interface";
 import { Student } from "../student/student.model";
 import { TUser } from "./user.interface";
 import { User } from "./user.model";
-import { generateFacultyId, generateStudentId } from "./user.utils";
+import { generateAdminId, generateFacultyId, generateStudentId } from "./user.utils";
 import AppError from "../../../errors/AppError";
 import { StatusCodes } from "http-status-codes";
 import { TFaculty } from "../Faculty/faculty.interface";
 import { AcademicDepartment } from "../academicDepartmentt/academicDepartment.model";
 import { Faculty } from "../Faculty/faculty.model";
+import { TAdmin } from "../Admin/admin.interface";
+import { Admin } from "../Admin/admin.model";
 
 const createStudentIntoDB = async(password:string,payload:TStudent)=>{
 
@@ -106,7 +109,43 @@ const createFacultyIntoDB = async(password:string, payload:TFaculty)=>{
   }
 
 }
+const createAdminIntoDB = async(password:string, payload:TAdmin)=>{
+  // create a user object
+  const userData:Partial<TUser>={};
+  //if password is not given, use default password
+  userData.password = password || (config.default_pass as string);
+  // set user role
+  userData.role = 'admin';
+  const session = await mongoose.startSession();
+  try{
+    session.startTransaction();
+    //set generated id
+    userData.id = await generateAdminId();
+     // create a user (transaction-1)
+     const newUser = await User.create([userData],{session});
+     
+     if(!newUser.length){
+      throw new AppError(StatusCodes.BAD_REQUEST,'Failed to create Admin');
+     }
+     // set id, _id as user
+     payload.id = newUser[0].id;
+     payload.user = newUser[0]._id;// refference _id
+     //create a admin(transaction-2)
+     const newAdmin = await Admin.create([payload],{session});
+     if(!newAdmin.length){
+      throw new AppError(StatusCodes.BAD_REQUEST,'Failed to create admin');
+     }
+     await session.commitTransaction();
+     await session.endSession();
+     return newAdmin;
+  }catch(err:any){
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(err);
+  }
+}
 export const UserServices={
     createStudentIntoDB ,
-    createFacultyIntoDB 
+    createFacultyIntoDB ,
+    createAdminIntoDB
 }
